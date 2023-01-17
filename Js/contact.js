2c3bff0f8f1098ef8ed2e392f34b8226b4dd4581
MahletTesfaye <mahlettenaye@gmail.com>
2023-01-18 01:35:39 +0300

    updated css and js final with contact validation

--- Js/contact.js
+++ Js/contact.js
@@ -11,14 +11,11 @@ const contact_form_phone = document.querySelector("#contact__form__phone");
 const phone_validator = document.querySelector(".contact__form__PhoneValidator");
 
 const contact__form__message_check = document.querySelector(".contact__form__message-check");
-const contact_form_message = document.querySelector("#contact__form__message");
-console.log("message");
-console.log(contact_form_submit)
-function formValidator(e) {
+const contact_form_message = document.querySelector("#contact__form__message").value;
+
+function formValidator() {
     console.log("message");
-    e.preventDefault();
     const name = contact_form_name.value;
-    const namePattern = /^[a-zA-Z]$/;
     const email = contact_form_email.value;
     const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const phone = contact_form_phone.value;
@@ -26,10 +23,6 @@ function formValidator(e) {
 
     if (name === "") {
         name_validator.innerHTML = "This field is required.";
-    }
-    
-    else if (!namePattern.test(name)) {
-        name_validator.innerHTML = "Invalid name";
     }
     else{
         name_validator.innerHTML = "";
@@ -54,16 +47,15 @@ function formValidator(e) {
     else{
         phone_validator.innerHTML = "";
     }
-    if (contact_form_message.textContent == "" ){
+    if (contact_form_message.textContent === "" ){
         contact__form__message_check.innerHTML = "Write a message";
     }
     else{
         contact__form__message_check.innerHTML = "";
     }
 
-    if (email_validator.innerHTML == "" && phone_validator.innerHTML == ""){
-        alert("Your message is successfully sent.")
+    if (email_validator.innerHTML === "" && phone_validator.innerHTML === "" && name_validator.innerHTML ==="" && contact__form__message_check.innerHTML === ""){
+        alert("Your message is successfully sent.");
     }
 }
-// contact_form_submit.addEventListener('click', formValidator);
-// contact_form_submit.addEventListener("click", ()=>{console.log("clicked")});
+contact_form_submit.addEventListener('click', formValidator);
--- Js/index.js
+++ Js/index.js
@@ -9,14 +9,7 @@ function barClick() {
         bar.style.display = "none";
         times.style.display = "block";
         main.style.marginTop = "155px";
-        // Search_Delivery.style.float = "left";
-        // SaronMB.style.clear = "left";
     }
-    // else {
-    //     header_nav.style.display = "none"
-    //     bar.style.display = "block";
-    //     header_nav.style.display = "none";
-    // }
 }
 
 function timesClick() {
@@ -25,19 +18,10 @@ function timesClick() {
     header_nav.style.display = "none";
     times.style.display = "none";
     main.style.marginTop = "0";
-    // Search_Delivery.style.float = "right"
-    // SaronMB.style.clear = "right";
+
+
 }
 
-
-// console.log(bar, "herer");
 bar.addEventListener('click', barClick, false)
 times.addEventListener('click', timesClick, false)
-
-
-
-
-
-
-
 
